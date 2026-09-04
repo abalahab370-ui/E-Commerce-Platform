a62d53e2f2b745490147d57c8b4b268899d2ce9d
Mokhfi Mohammed <abalahab370@gmail.com>
2026-09-04 21:42:37 +0100

    fixed deleteProduct feature

--- Controllers/deleteProductController.js
+++ Controllers/deleteProductController.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose") ;
 const Product = require("../models/products") ;
+const cloudinary = require("../config/cloudinary") ;
 
 
 const deleteProduct = async (req , res) => {
@@ -10,13 +11,27 @@ const deleteProduct = async (req , res) => {
             const product = await Product.findByIdAndDelete(productId) ;
 
             if (product) {
+                  //well we have to delete the related images too : 
+                  if (product.images && product.images.length > 0) {
+                  const imagesUrls = product.images.map((image) => {
+                        return image.publicId ;
+                  }) ;
+
+                  const DeletePromises = imagesUrls.map( (url) => {
+                        return cloudinary.uploader.destroy(url) ;
+                  })
+
+                  const result = await Promise.all(DeletePromises) ;
+                  }
+
                   return res.status(200).json({ 'message' : 'Product Deleted Successfully'}) ;
             } else {
                   return res.status(404).json( {'message' : 'Product Does not Exist'})
             }
 
       } catch (err) {
-            console.error(`Sir we have an error in Deleting Products : ${err}`)
+            console.error(`Sir we have an error in Deleting Products : ${err}`) ;
+            return res.status(500).json({ 'message': 'Internal Server Error' }) ;
       }
 }
 
