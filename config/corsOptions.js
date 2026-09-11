33bbce4d1f5158b99f546a3b51f908bdae1ced8a
Mokhfi Mohammed <abalahab370@gmail.com>
2026-09-11 17:10:59 +0100

    added the front end url to cors configs

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -3,7 +3,8 @@ const cors = require("cors") ;
 //Cross Origin Resource Sharing :
 const whitelist = [
       'http://127.0.0.1:5500',
-      'http://127.0.0.1:5173'
+      'http://127.0.0.1:5173' ,
+      'https://storedz-ui.onrender.com'
 ];
 
 const corsOptions = {
